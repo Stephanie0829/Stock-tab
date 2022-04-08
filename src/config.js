c3d37a668e35be271904fd4df4cd071b48c41f33
Stephanie Wei <72533858+Stephanie0829@users.noreply.github.com>
2022-04-08 11:59:34 -0700

    Remove keys for security.

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const NEWS_API_KEY = "518a677af3ec4a76a0643af0e786cff7";
-export const MARKET_API_KEY = "gQpDQMV5xZqzdT2lFqhub4591homJ0yp";
-export const STOCK_API_KEY = "gQpDQMV5xZqzdT2lFqhub4591homJ0yp";
+export const NEWS_API_KEY = "INSERTKEY";
+export const MARKET_API_KEY = "INSERTKEY";
+export const STOCK_API_KEY = "INSERTKEY";
